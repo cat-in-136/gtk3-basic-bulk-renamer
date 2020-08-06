4cea7afb5ef5a0d9465f4b9a1f69b1204cf4f170
cat_in_136 <cat.in.136+github@gmail.com>
2020-08-07 00:51:42 +0900

    introduce undo and refactor incl. renaming

--- src/bulk_renamer.rs
+++ src/bulk_renamer.rs
@@ -7,22 +7,23 @@ use tempfile::NamedTempFile;
 
 pub type RenameMapPair = (PathBuf, PathBuf);
 
-/// Renaming mapping pair with source name and target name.
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub struct RenameMap {
-    pub pairs: Vec<RenameMapPair>,
-}
-
 #[derive(Clone, Copy)]
-pub enum RenameMapOverwriteMode {
+pub enum RenameOverwriteMode {
     ChangeFileName,
     Overwrite,
     Error,
 }
 
-impl RenameMap {
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct BulkRename {
+    pub pairs: Vec<RenameMapPair>,
+    undo_pairs: Option<Vec<RenameMapPair>>,
+}
+
+impl BulkRename {
     pub fn new(pairs: Vec<RenameMapPair>) -> Self {
-        Self { pairs }
+        let undo_pairs = Some(Vec::with_capacity(pairs.len()));
+        Self { pairs, undo_pairs }
     }
 
     pub fn fix_target_file_path(target: &PathBuf) -> Result<PathBuf, RenameError> {
@@ -58,11 +59,14 @@ impl RenameMap {
         Ok(())
     }
 
-    pub fn execute(&self, over_write_mode: RenameMapOverwriteMode) -> Result<(), RenameError> {
+    pub fn execute(&mut self, over_write_mode: RenameOverwriteMode) -> Result<(), RenameError> {
+        if self.undo_pairs.as_ref().map_or(true, |v| v.len() > 0) {
+            return Err(RenameError::Executed);
+        }
         self.check_not_found_source_files()?;
 
         // Step 1 Move the all files to temporary name.
-        let mut temp_files = Vec::with_capacity(self.pairs.len());
+        let mut temp_filenames = Vec::with_capacity(self.pairs.len());
         for pair in self.pairs.iter() {
             let target_parent = pair.1.parent().ok_or(RenameError::IllegalOperation)?;
             let target_temp_filename = {
@@ -75,30 +79,46 @@ impl RenameMap {
 
             fs::rename(&pair.0, &target_temp_filename)
                 .map_err(|error| RenameError::IoError(pair.clone(), error))?;
-            temp_files.push(target_temp_filename);
+            if let Some(undo_pairs) = self.undo_pairs.as_mut() {
+                undo_pairs.push((target_temp_filename.clone(), pair.0.clone()));
+            }
+            temp_filenames.push(target_temp_filename);
         }
 
         // Step 2 Move them to target
         for (i, pair) in self.pairs.iter().enumerate() {
-            if pair.0 != pair.1 {
-                let target_file = match over_write_mode {
-                    RenameMapOverwriteMode::ChangeFileName => Self::fix_target_file_path(&pair.1),
-                    RenameMapOverwriteMode::Overwrite => Ok(pair.1.clone()),
-                    RenameMapOverwriteMode::Error => {
-                        if pair.1.exists() {
-                            Err(RenameError::TargetFileAlreadyExists(pair.clone()))
-                        } else {
-                            Ok(pair.1.clone())
-                        }
+            let target_temp_file = &temp_filenames[i];
+            let target_file = match over_write_mode {
+                RenameOverwriteMode::ChangeFileName => Self::fix_target_file_path(&pair.1),
+                RenameOverwriteMode::Overwrite => {
+                    if pair.1.exists() {
+                        self.undo_pairs = None; // Mark not undoable
                     }
-                }?;
+                    Ok(pair.1.clone())
+                }
+                RenameOverwriteMode::Error => {
+                    if pair.1.exists() {
+                        Err(RenameError::TargetFileAlreadyExists(pair.clone()))
+                    } else {
+                        Ok(pair.1.clone())
+                    }
+                }
+            }?;
 
-                fs::rename(&temp_files[i], &target_file)
-                    .map_err(|error| RenameError::IoError(pair.clone(), error))?;
+            fs::rename(target_temp_file, &target_file)
+                .map_err(|error| RenameError::IoError(pair.clone(), error))?;
+            if let Some(undo_pairs) = self.undo_pairs.as_mut() {
+                undo_pairs[i].0 = target_file;
             }
         }
 
         Ok(())
+    }
+
+    pub fn undo_bulk_rename(&self) -> Option<BulkRename> {
+        self.undo_pairs
+            .as_ref()
+            .map(|undo_pairs| BulkRename::new(undo_pairs.clone()))
     }
 }
 
@@ -123,7 +143,9 @@ mod test {
             fs::write(&a_ith_txt, "a").unwrap();
 
             assert_eq!(
-                RenameMap::fix_target_file_path(&a_txt).unwrap().file_name(),
+                BulkRename::fix_target_file_path(&a_txt)
+                    .unwrap()
+                    .file_name(),
                 Some(OsString::from(format!("{}a.txt", "_".repeat(i + 1))).as_os_str())
             );
         }
@@ -132,9 +154,9 @@ mod test {
     #[test]
     pub fn test_execute_when_conflicting() {
         for &mode in &[
-            RenameMapOverwriteMode::ChangeFileName,
-            RenameMapOverwriteMode::Overwrite,
-            RenameMapOverwriteMode::Error,
+            RenameOverwriteMode::ChangeFileName,
+            RenameOverwriteMode::Overwrite,
+            RenameOverwriteMode::Error,
         ] {
             let temp_dir = tempfile::tempdir().unwrap();
 
@@ -144,24 +166,29 @@ mod test {
             fs::write(&file2_path, "2").unwrap();
             let rename_pair = (file1_path.clone(), file2_path.clone());
 
-            let rename = RenameMap::new(vec![rename_pair]);
+            let mut rename = BulkRename::new(vec![rename_pair]);
             let result = rename.execute(mode);
+            let undo_pairs = rename.undo_pairs;
 
             match mode {
-                RenameMapOverwriteMode::ChangeFileName => {
+                RenameOverwriteMode::ChangeFileName => {
                     let new_file_path = path_buf_join(temp_dir.path(), "_2.txt");
-                    assert_eq!(fs::read_to_string(new_file_path).unwrap(), "1");
+                    assert_eq!(fs::read_to_string(&new_file_path).unwrap(), "1");
                     assert_eq!(fs::read_to_string(&file2_path).unwrap(), "2");
+                    assert_eq!(undo_pairs, Some(vec![(new_file_path, file1_path)]));
                 }
-                RenameMapOverwriteMode::Overwrite => {
+                RenameOverwriteMode::Overwrite => {
                     let file2_path = path_buf_join(temp_dir.path(), "2.txt");
                     assert_eq!(fs::read_to_string(&file2_path).unwrap(), "1");
+                    assert_eq!(undo_pairs, None);
                 }
-                RenameMapOverwriteMode::Error => {
+                RenameOverwriteMode::Error => {
                     assert!(matches!(
                         result,
                         Err(RenameError::TargetFileAlreadyExists(_pair))
                     ));
+                    assert!(matches!(&undo_pairs, Some(_vec)));
+                    assert_eq!(undo_pairs.as_ref().unwrap()[0].1, file1_path);
                 }
             }
         }
@@ -171,21 +198,25 @@ mod test {
     pub fn test_execute_with_across_directories() {
         let temp_dir = tempfile::tempdir().unwrap();
         let temp_dir2 = tempfile::tempdir().unwrap();
+        let mut undo_pairs = Vec::new();
         let mut pairs = Vec::new();
         for i in 0..20 {
             let source_path = path_buf_join(temp_dir.path(), format!("{}.txt", i));
             let target_path = path_buf_join(temp_dir2.path(), format!("foobar_{}.txt", i));
             fs::write(&source_path, format!("{}", i)).unwrap();
+            undo_pairs.push((target_path.clone(), source_path.clone()));
             pairs.push((source_path, target_path));
         }
 
-        let rename = RenameMap::new(pairs);
-        rename.execute(RenameMapOverwriteMode::Error).unwrap();
+        let mut rename = BulkRename::new(pairs);
+        rename.execute(RenameOverwriteMode::Error).unwrap();
 
         for i in 0..20 {
             let target_path = path_buf_join(temp_dir2.path(), format!("foobar_{}.txt", i));
             assert_eq!(fs::read_to_string(target_path).unwrap(), format!("{}", i));
         }
+
+        assert_eq!(rename.undo_pairs, Some(undo_pairs));
     }
 
     #[test]
@@ -199,11 +230,19 @@ mod test {
             pairs.push((source_path, target_path));
         }
 
-        let rename = RenameMap::new(pairs);
-        rename.execute(RenameMapOverwriteMode::Error).unwrap();
+        let mut rename = BulkRename::new(pairs);
+        rename.execute(RenameOverwriteMode::Error).unwrap();
 
         for i in 0..20 {
             let target_path = path_buf_join(temp_dir.path(), format!("{}.txt", i + 1));
+            assert_eq!(fs::read_to_string(target_path).unwrap(), format!("{}", i));
+        }
+
+        let mut undo = rename.undo_bulk_rename().unwrap();
+        undo.execute(RenameOverwriteMode::Error).unwrap();
+
+        for i in 0..20 {
+            let target_path = path_buf_join(temp_dir.path(), format!("{}.txt", i));
             assert_eq!(fs::read_to_string(target_path).unwrap(), format!("{}", i));
         }
     }
@@ -211,6 +250,7 @@ mod test {
 
 #[derive(Debug)]
 pub enum RenameError {
+    Executed,
     SourceFileNotFound(Vec<RenameMapPair>),
     TargetFileAlreadyExists(RenameMapPair),
     TargetDirectoryNotWritable(RenameMapPair, IoError),
@@ -221,6 +261,7 @@ pub enum RenameError {
 impl error::Error for RenameError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
+            RenameError::Executed => None,
             RenameError::SourceFileNotFound(_) => None,
             RenameError::TargetFileAlreadyExists(_) => None,
             RenameError::TargetDirectoryNotWritable(_, error) => Some(error),
@@ -233,6 +274,7 @@ impl error::Error for RenameError {
 impl fmt::Display for RenameError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
+            RenameError::Executed => f.write_str("Already Executed"),
             RenameError::SourceFileNotFound(pairs) => f.write_fmt(format_args!(
                 "Source Not Found: {}",
                 pairs
