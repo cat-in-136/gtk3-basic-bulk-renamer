a6b477919638d500ef0c0d2da98c7ac4b26184f7
cat_in_136 <cat.in.136+github@gmail.com>
2020-08-07 19:42:12 +0900

    introduce clear button

--- gtk-basic-bulk-renamer/src/win/window.rs
+++ gtk-basic-bulk-renamer/src/win/window.rs
@@ -1,8 +1,9 @@
+use gio::{ActionMapExt, ApplicationExt, SimpleAction};
 use gtk::prelude::*;
+use gtk::ListStore;
 use gtk::{Application, TreeView};
 use gtk::{ApplicationWindow, Builder, GtkWindowExt};
 use std::path::{Path, PathBuf};
-use gtk::ListStore;
 
 const ID_ADD_BUTTON: &'static str = "add-button";
 const ID_CLEAR_BUTTON: &'static str = "clear-button";
@@ -19,10 +20,11 @@ pub(crate) struct Window {
 }
 
 impl Window {
-
     pub fn new(app: &Application) -> Self {
         let builder = Builder::from_string(include_str!("window.glade"));
         let window = Self { builder };
+
+        window.init_action(app);
 
         let main_window = window.main_window();
         main_window.set_application(Some(app));
@@ -30,23 +32,38 @@ impl Window {
         window
     }
 
+    fn get_cloned_object<T: IsA<glib::Object>>(&self, name: &str) -> T {
+        let object: T = self.builder.get_object(name).unwrap();
+        object.clone()
+    }
+
+    fn init_action(&self, _app: &Application) {
+        let main_window = self.main_window();
+        let clear_action = SimpleAction::new("clear-action", None);
+        {
+            let file_list_store = self.get_cloned_object::<ListStore>(ID_FILE_LIST_STORE);
+            clear_action.connect_activate(move |_, _| {
+                file_list_store.clear();
+            });
+        }
+        main_window.add_action(&clear_action);
+    }
+
     pub fn set_files(&self, paths: &[PathBuf]) {
-        let file_list_store: ListStore = self.builder.get_object(ID_FILE_LIST_STORE).unwrap();
+        let file_list_store = self.get_cloned_object::<ListStore>(ID_FILE_LIST_STORE);
         file_list_store.clear();
 
         for path in paths.iter() {
             let iter = file_list_store.append();
-            file_list_store.set(&iter, &[0, 1], &[
-                &path.display().to_string(),
-                &path.display().to_string(),
-            ]);
+            file_list_store.set(
+                &iter,
+                &[0, 1],
+                &[&path.display().to_string(), &path.display().to_string()],
+            );
         }
-
     }
 
     pub fn main_window(&self) -> ApplicationWindow {
-        self.builder.get_object(ID_MAIN_WINDOW).unwrap()
+        self.get_cloned_object(ID_MAIN_WINDOW)
     }
 }
-
-
