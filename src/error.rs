7f6b5783b1024cbd124d36ad2e1eac3898b30ac8
cat_in_136 <cat.in.136+github@gmail.com>
2020-12-30 07:46:48 +0900

    introduce thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,40 +1,11 @@
 use crate::basic_bulk_renamer::RenameError;
 use regex::Error as RegexError;
-use std::error;
-use std::fmt;
+use thiserror;
 
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub(crate) enum Error {
-    Rename(RenameError),
-    Regex(RegexError),
-}
-
-impl error::Error for Error {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match self {
-            Error::Rename(e) => Some(e),
-            Error::Regex(e) => Some(e),
-        }
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::Rename(e) => e.fmt(f),
-            Error::Regex(e) => e.fmt(f),
-        }
-    }
-}
-
-impl From<RenameError> for Error {
-    fn from(e: RenameError) -> Self {
-        Self::Rename(e)
-    }
-}
-
-impl From<RegexError> for Error {
-    fn from(e: RegexError) -> Self {
-        Self::Regex(e)
-    }
+    #[error(transparent)]
+    Rename(#[from] RenameError),
+    #[error(transparent)]
+    Regex(#[from] RegexError),
 }
