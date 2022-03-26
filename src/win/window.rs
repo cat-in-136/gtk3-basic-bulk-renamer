354bba4087b7fcfffe446b5728f59822e8862dd4
cat_in_136 <cat.in.136+github@gmail.com>
2022-03-26 13:15:13 +0900

    autoresize the file list columns on empty
    to fix cat-in-136/gtk3-basic-bulk-renamer#4 where the window width cannot be shrunk after enlarged.

--- src/win/window.rs
+++ src/win/window.rs
@@ -192,21 +192,20 @@ impl Window {
         main_window.add_action(&execute_action);
 
         selection.connect_changed(glib::clone!(
-                @weak file_list_store,
-                @weak file_list,
-                @weak selection,
-                @weak remove_action,
-                @weak clear_action,
-                @weak execute_action => move |_| {
-                let file_list_store_count = file_list_store.iter_n_children(None);
-                if file_list_store_count == 0 {
-                    file_list.columns_autosize();
-                }
-                remove_action.set_enabled(selection.count_selected_rows() > 0);
-                clear_action.set_enabled(file_list_store_count > 0);
-                execute_action.set_enabled(file_list_store_count > 0);
+            @weak file_list_store,
+            @weak file_list,
+            @weak selection,
+            @weak remove_action,
+            @weak clear_action,
+            @weak execute_action => move |_| {
+            let file_list_store_count = file_list_store.iter_n_children(None);
+            if file_list_store_count == 0 {
+                file_list.columns_autosize();
             }
-        ));
+            remove_action.set_enabled(selection.count_selected_rows() > 0);
+            clear_action.set_enabled(file_list_store_count > 0);
+            execute_action.set_enabled(file_list_store_count > 0);
+        }));
         file_list_store.connect_row_inserted(glib::clone!(@weak selection => move |_, _, _| {
             selection.emit("changed", &[]).ok();
         }));
